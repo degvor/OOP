1a708cbab660ff8f5289cf5b4554fcfdaf4369ee
Andrey <89276859+degvor@users.noreply.github.com>
2022-02-22 10:06:42 +0200

    Add files via upload

---
+++ Lab1_Sharp/Lab1_Sharp/Display.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_Sharp
+{
+    internal class Display
+    {
+        public void PrintFromFile(string path)
+        {
+            StreamReader streamReader = new StreamReader(path);
+            Console.WriteLine($"\nFile name is {path}");
+            Console.WriteLine(streamReader.ReadToEnd());
+            streamReader.Close();
+        }
+    }
+}
---
+++ Lab1_Sharp/Lab1_Sharp/File_creator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_Sharp
+{
+    internal class File_creator
+    {
+        public void Generate_file(string path)
+        {
+            bool check = true;
+            string all = "";
+            Console.WriteLine("Введите данные");
+            while (check)
+            {
+                all = Console.ReadLine() + '\n';
+                System.IO.File.AppendAllText(path, all);
+                ConsoleKeyInfo key = Console.ReadKey(true);
+                if (key.Key == ConsoleKey.F10)
+                {
+                    check = false;
+                }
+            }
+        }
+    }
+}

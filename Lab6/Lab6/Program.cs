448131afdb67d0f34de51366a7ee7ccdfbb10bb9
Andrey <89276859+degvor@users.noreply.github.com>
2022-06-14 01:21:00 +0300

    Add files via upload

---
+++ Lab6/Lab6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Security.Claims;
+
+namespace Lab6
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Tree tree = new Tree(-8);
+            int[] massiv = new[] {-1, 5, -7, 8 ,9};
+            foreach (var item in massiv)
+            {
+                tree.Insert(item);
+            }
+
+            Console.WriteLine(tree.PreorderTraverse());
+            Console.WriteLine(tree.CountNegative());
+            Console.WriteLine(tree.CountPositive());
+            Console.ReadLine();
+        }
+    }
+}
